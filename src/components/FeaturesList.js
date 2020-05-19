f2612c976bb29d2aff602494848c19bb5aabce44
Genairix <mblalock90@yahoo.com>
2020-05-19 10:16:43 -0400

    Add files via upload

---
+++ src/components/FeaturesList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Feature from './Features';
+
+class FeaturesList extends React.Component { 
+
+    render () {
+
+        const features = Object.keys(this.props.features).map((feature, idx) => {
+            const featureHash = feature + '-' + idx;
+
+            return <Feature key={featureHash} currentFeature={feature} features={this.props.features} updateFeature={this.props.updateFeature} selected={this.props.selected} />
+          });
+
+
+        return (
+        <form className="main__form">
+            <h2>Customize your laptop</h2>
+            {features}
+        </form>
+        )
+    }
+}
+
+export default FeaturesList;
---
+++ src/components/Summary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import CartEntry from './CartEntry';
+import Total from './Total';
+
+class Summary extends React.Component { 
+
+    render() {
+        const summary = Object.keys(this.props.selected).map((feature, idx) => {
+            const featureHash = feature + '-' + idx;
+            const selectedOption = this.props.selected[feature];
+            
+            return <CartEntry key={featureHash} selectedOption={selectedOption} currentFeature={feature} />
+          });
+
+        return (
+            <section className="main__summary">
+                <h2>Your cart</h2>
+                {summary}
+                <Total total={this.props.total}/>
+            </section>
+        )
+    }
+}
+
+export default Summary;
